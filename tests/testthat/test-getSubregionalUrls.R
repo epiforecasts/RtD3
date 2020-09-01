85d167b502b3bb668397b20a4a4d85367b0ec81d
seabbs <s.e.abbott12@gmail.com>
2020-09-01 15:04:21 +0000

    squash bug in getSubregionalUrls

--- tests/testthat/test-getSubregionalUrls.R
+++ tests/testthat/test-getSubregionalUrls.R
@@ -2,9 +2,9 @@ context("getSubregionalUrls")
 
 test_that("Test that getSubregionalUrls can construct the expected output", {
   out <- getSubregionalUrls(path = "hi/",
-                            areas = c('there', 'their'))
+                            areas = c('there', 'their', "over there"))
 
 
-  expect_equal(names(out), c("there", "their"))
-  expect_equal(unname(out), c("hi/there/","hi/their/"))
+  expect_equal(names(out), c("there", "their", "over there"))
+  expect_equal(unname(out), c("hi/there/","hi/their/", "hi/over-there"))
 })
