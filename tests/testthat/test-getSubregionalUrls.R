5d550a21bcd989e5665110a944c2fb3d94a7fc14
hamishgibbs <hamishgibbs@gmail.com>
2020-09-02 07:15:45 +0100

    Update tests

--- tests/testthat/test-getSubregionalUrls.R
+++ tests/testthat/test-getSubregionalUrls.R
@@ -2,8 +2,8 @@ context("getSubregionalUrls")
 
 testthat::test_that("Test that getSubregionalUrls can construct the expected output", {
   out <- getSubregionalUrls(path = "hi/",
-                            areas = c('there', 'their', "over there"))
+                            areas = c('there', 'Their', "over there"))
 
-  testthat::expect_equal(names(out), c("there", "their", "over there"))
+  testthat::expect_equal(names(out), c("there", "Their", "over there"))
   testthat::expect_equal(unname(out), c("hi/there/","hi/their/", "hi/over-there/"))
 })
