0133d7fe4d27766c36eec784475cf20dbac49933
hamishgibbs <hamishgibbs@gmail.com>
2020-09-18 17:34:20 +0100

    mapWidget

--- R/mapWidget.R
+++ R/mapWidget.R
@@ -2,10 +2,8 @@
 #'
 #' @description Create a map of Rt summary data
 #'
+#' @param geoData sf object, map data
 #' @param rtData data.frame, rt estimates in the format {'Source':{'rtData':x, 'casesInfectionData':x, 'casesReportData':x, 'obsCasesData':x}, ...}
-#' @param activeArea character, the default area to plot.
-#' @param activeTime character, the default time window (defaults to 'all')
-#' @param runDate character, date of estimate run in the format ('YYYY-MM-DD')
 #' @param width integer, width in pixels
 #' @param elementId string, id of element
 #' @param dryRun Logical, defaults to FALSE. Should the function be tested without the widget being created.
@@ -14,30 +12,41 @@
 #'
 #' @export
 
-tsWidget <- function(rtData = NULL,
-                     activeArea = NULL,
-                     activeTime = 'all',
-                     runDate = NULL,
+mapWidget <- function(geoData = NULL,
+                     rtData = NULL,
                      width = 900,
                      elementId = NULL,
                      dryRun = FALSE) {
-  
+
   arg_types <- sapply(ls(), function(x){return(class(get(x)))})
-  
-  invisible(check_input_data(arg_types = arg_types, geoData = NULL, rtData = rtData))
-  
+
+  invisible(check_input_data(arg_types = arg_types, geoData = geoData, rtData = rtData))
+
   #define height, which is fixed based on dataset availability
-  height <- define_height(geoData = NULL, rtData = rtData)
-  
+  height <- define_height(geoData = geoData, rtData = rtData, map_only = T)
+
+  rtData_null <- list('summaryData' = NULL, 'rtData' = NULL, 'casesInfectionData' = NULL, 'casesReportData' = NULL, 'obsCasesData' = NULL)
+
+  rtData_replacement <- list()
+
+  for (source in names(rtData)){
+
+    rtData_null$summaryData <- rtData[[source]]$summaryData
+
+    rtData_replacement[[source]] <- rtData_null$summaryData
+  }
+
+  print(rtData_replacement)
+
   x = list(
-    activeArea = activeArea,
-    activeTime = activeTime,
-    runDate = runDate,
-    geoData = NULL,
-    rtData = jsonNull(rtData),
+    activeArea = NULL,
+    activeTime = NULL,
+    runDate = NULL,
+    geoData = geojsonNull(geoData),
+    rtData = jsonNull(rtData_replacement),
     subregional_ref = NULL
   )
-  
+
   if (!dryRun) {
     # create widget
     htmlwidgets::createWidget(
@@ -51,5 +60,5 @@ tsWidget <- function(rtData = NULL,
   }else{
     return(TRUE)
   }
-  
+
 }
