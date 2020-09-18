c9f31dcc1e9a69e6d2bc489ed9c6c8b5ccf4523d
hamishgibbs <hamishgibbs@gmail.com>
2020-09-18 16:18:41 +0100

    alter tests

--- tests/testthat/test-tsWidget.R
+++ tests/testthat/test-tsWidget.R
@@ -1,8 +1,5 @@
 testthat::test_that('tsWidget errors as expected', {
   
-  correct_rtData = list()
-  incorrect_rtData = 'a'
-  
   rt <- list('a'=list('a' = 'a', 'casesInfectionData' = 'a', 'casesReportData' = 'a', "obsCasesData" = 'a', 'summaryData' = 'a'),
              'b'=list('rtData' = 'a', 'casesInfectionData' = 'a', 'casesReportData' = 'a', "obsCasesData" = 'a', 'summaryData' = 'a'))
   
